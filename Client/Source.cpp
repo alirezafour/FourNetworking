3118a8304e5417bc3d5bf3b372ca47d719adc200
alirezafour <alirezafour@gmail.com>
2022-09-12 23:52:56 +0300

    basic server/client

--- Client/Source.cpp
+++ Client/Source.cpp
@@ -1,8 +1,47 @@
+#include <boost/asio.hpp>
 #include <iostream>
-#include "FourNetworking/FourNetworking.h"
+#include <array>
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello from Client!.\n";
+	try
+	{
+		boost::asio::io_context io_context;
+
+		boost::asio::ip::tcp::resolver resolver{ io_context };
+
+		boost::asio::ip::tcp::resolver::results_type endpoints = resolver.resolve("127.0.0.1", "13");
+
+		boost::asio::ip::tcp::socket socket{ io_context };
+		boost::asio::connect(socket, endpoints);
+
+		while (true)
+		{
+			// Listen for messages
+			std::array<char, 128> buffer;
+			boost::system::error_code errorCode;
+
+			size_t len = socket.read_some(boost::asio::buffer(buffer), errorCode);
+
+			if (errorCode == boost::asio::error::eof)
+			{
+				// clean connection cut off
+				break;
+			}
+			else if(errorCode)
+			{
+				throw boost::system::system_error(errorCode);
+			}
+
+			std::cout.write(buffer.data(), len);
+		}
+	}
+
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << "\n";
+	}
+
+	std::cin.get();
 	return 0;
 }
--- Server/Source.cpp
+++ Server/Source.cpp
@@ -1,8 +1,32 @@
+#include <boost/asio.hpp>
 #include <iostream>
-#include "FourNetworking/FourNetworking.h"
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello form server!\n";
+	try {
+		boost::asio::io_context io_context;
+
+		boost::asio::ip::tcp::acceptor acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 1337));
+
+		while (true)
+		{
+			std::cout << "accepting connections! \n";
+			boost::asio::ip::tcp::socket socket(io_context);
+			acceptor.accept(socket);
+
+			std::cout << "Client Connected! Sending message!\n";
+
+			std::string hello_message = "Hello, my client!\n";
+			boost::system::error_code errorCode;
+
+			boost::asio::write(socket, boost::asio::buffer(hello_message), errorCode);
+		}
+
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << "\n";
+	}
+	
 	return 0;
 }
